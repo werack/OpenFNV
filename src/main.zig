4d67aecf1bf45dee3546945a7c9fe00ddf5f3dc7
Werack <werack@tutamail.com>
2026-03-02 12:24:44 +0200

    create a simple window in imgui

--- src/main.zig
+++ src/main.zig
@@ -7,28 +7,37 @@ const BSA = @import("loaders/bsa.zig");
 const c = @import("lib/lib.zig").zimgui;
 
 pub fn main(init: std.process.Init) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
     var window = try Window.init(640, 480, "OpenFNV");
     defer window.deinit();
+
+    var paths = try BSA.load("assets/Fallout - Meshes.bsa", init.io, allocator);
+    defer paths.?.deinit(allocator);
 
     while (window.shouldClose() == false) {
         window.beginDraw();
 
-        // var open: bool = true;
-        //_ = c.ImGui_Begin("BSA Explorer", @ptrCast(&open), c.ImGuiWindowFlags_MenuBar);
-        //if (c.ImGui_BeginMenuBar()) {
-        //    if (c.ImGui_BeginMenu("File")) {
-        //        if (c.ImGui_MenuItem("Open")) {}
-        //
-        //        c.ImGui_EndMenu();
-        //    }
-        //    c.ImGui_EndMenuBar();
-        //}
-        //
-        //c.ImGui_End();
-        c.ImGui_ShowDemoWindow(@constCast(&true));
+        _ = c.ImGui_Begin("BSA Explorer", @constCast(&true), c.ImGuiWindowFlags_MenuBar);
+        if (c.ImGui_BeginMenuBar()) {
+            if (c.ImGui_BeginMenu("File")) {
+                // menu bar
+                if (c.ImGui_MenuItem("Open")) {}
+                if (c.ImGui_MenuItem("Close")) {}
+
+                c.ImGui_EndMenu();
+            }
+            c.ImGui_EndMenuBar();
+        }
+
+        if (c.ImGui_TreeNode("test")) {
+            c.ImGui_TreePop();
+        }
+
+        c.ImGui_End();
 
         window.endDraw();
     }
-
-    _ = init;
 }
